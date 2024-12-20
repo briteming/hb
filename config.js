5b3da8a09121552a60aa32936daf6378aab226d8
briteming <briteming@gmail.com>
2024-12-20 10:33:27 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 config = {
-    blog_name: "涵曦 ~ 博客",
-    github_username: "hanxi", // github 账号名
-    github_repo: "blog",      // github 建立的 repo 名
-    per_page: 10,             // 首页每次载入文章列表数量
+    blog_name: "brite的博客",
+    github_username: "briteming", // github 账号名
+    github_repo: "hb",      // github 建立的 repo 名
+    per_page: 5,             // 首页每次载入文章列表数量
 
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
-    access_token: "g"+"hp_djsCiXH8lEL4HBOD1R7yS"+"DiJoZDPI31fdtRw"
+   // access_token: "g"+"hp_djsCiXH8lEL4HBOD1R7yS"+"DiJoZDPI31fdtRw"
 };
